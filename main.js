3faad0ce2388a2cebbcf629a87d316296225b60f
Michael Dotson <thedotson@icloud.com>
2020-05-12 18:09:45 -0500

    closes #18

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("is this thing on?")
+
+const printToDom = (selector, textToPrint) => {
+  const selectedDiv = document.querySelector(selector);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const clickEvents = () => {
+
+}
+
+const init = () => {
+  clickEvents();
+}
+
+init();
