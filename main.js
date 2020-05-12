1b852b8fc3313c8be84b10c83c562712e285a642
Michael Dotson <thedotson@icloud.com>
2020-05-12 18:53:52 -0500

    closes #10

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 console.log("is this thing on?")
 
+const articles = [
+  {
+    headline: "J-Law in Flowy Culottes",
+    articleImage: 'https://bit.ly/2WKKlbi',
+    publishDate: '01/15/20',
+    articleURL: 'https://bit.ly/2WqQZod',
+  },
+  {
+    headline: "Mila Kunis in Wide-Leg Pants",
+    articleImage: 'https://bit.ly/3dGj8gC',
+    publishDate: '12/10/19',
+    articleURL: 'https://bit.ly/2WT6m7F',
+  },
+  {
+    headline: "Kate Middleton repping the 'lottes",
+    articleImage: 'https://bit.ly/2YWXmBl',
+    publishDate: '10/10/19',
+    articleURL: 'https://bit.ly/2LpD3UY',
+  },
+];
+
 const printToDom = (selector, textToPrint) => {
   const selectedDiv = document.querySelector(selector);
   selectedDiv.innerHTML = textToPrint;
+}
+
+const articleCardBuilder = (arr) => {
+  domString = '';
+
+  for (let i = 0; i < arr.length; i++) {
+      domString += `
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${arr[i].headline}</h5>
+        <p class="card-text">${arr[i].publishDate}</p>
+        <img class="card-img-top" src="${arr[i].articleImage}" alt="">
+      </div>
+    </div>
+    `;
+  }
+  printToDom('#articleCards', domString)
 }
 
 const clickEvents = () => {
@@ -10,6 +48,7 @@ const clickEvents = () => {
 }
 
 const init = () => {
+  articleCardBuilder(articles);
   clickEvents();
 }
 
