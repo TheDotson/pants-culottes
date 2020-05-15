bfa84b6dd7ce38dac886bf1d8c8a63aae830a18e
Nick Walters <nswalters.s@gmail.com>
2020-05-14 21:52:11 -0500

    Formatting fixes

--- main.js
+++ main.js
@@ -1,134 +1,117 @@
 console.log("is this thing on?")
 
-const pants01 = [
-  {
-      id: 00,
-      name: "Georgette Wide-Leg Pants",
-      nameD: "St John Collection georgette layered pants",        
-      price:150.00,
-      model:"History Culotte",
-      size: [2,4,6],
-      imgUrl:
-      [
-          {
-              color: "black",
-              url:"https://images.neimanmarcus.com/ca/6/product_assets/B/2/Z/D/Q/NMB2ZDQ_mz.jpg"
-          },
-          {
-              color:"black",
-              url:"hhtp..."
-          }
-      ],
-      disc: "St John Collection georgette layered pants. Banded waist. Wide relaxed-fit legs.",
-      productURL: "https://www.neimanmarcus.com/p/st-john-collection-georgette-wide-leg-pants-prod179760472",
+const pants01 = [{
+    id: 00,
+    name: "Georgette Wide-Leg Pants",
+    nameD: "St John Collection georgette layered pants",
+    price: 150.00,
+    model: "History Culotte",
+    size: [2, 4, 6],
+    imgUrl: [{
+      color: "black",
+      url: "https://images.neimanmarcus.com/ca/6/product_assets/B/2/Z/D/Q/NMB2ZDQ_mz.jpg"
+    }],
+    disc: "St John Collection georgette layered pants. Banded waist. Wide relaxed-fit legs.",
+    productURL: "https://www.neimanmarcus.com/p/st-john-collection-georgette-wide-leg-pants-prod179760472",
 
   },
   {
-      id: 01,
-      name: "G-Star RAW Women High Waist Culotte Sweatpants",
-      nameD: "",        
-      price:130.00,
-      model:"Fashion Culotte",
-      size: [2,4,6],
-      imgUrl:
-      [
-          {
-              color: "grey",
-              url:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQcMmej_GtcHOLlhwagZ2HzPqtkoXLrp089HdagzwnI0qszQsBDMLXcqq-4RZjWOrSx2SltJU4&usqp=CAc"
-          },
-          {
-              color:"light skin",
-              url:"https://img1.g-star.com/product/c_fill,f_auto,h_675,q_80/v1583927558/D17299-C332-B436-Z01W/g-star-raw-high-waist-culotte-sweatpants-pink.jpg"
-          }
-      ],
-      disc: "A broad elasticated waist offers an integrated drawstring. The High Waist Culotte sweatpants is designed in a wide flared fit and a high waist. Inset pockets are added. The leghems show a raw edge finish.",
-      productURL: "https://www.g-star.com/en_us/shop/women/pants/d17299-c332-b436",
+    id: 01,
+    name: "G-Star RAW Women High Waist Culotte Sweatpants",
+    nameD: "",
+    price: 130.00,
+    model: "Fashion Culotte",
+    size: [2, 4, 6],
+    imgUrl: [{
+        color: "grey",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQcMmej_GtcHOLlhwagZ2HzPqtkoXLrp089HdagzwnI0qszQsBDMLXcqq-4RZjWOrSx2SltJU4&usqp=CAc"
+      },
+      {
+        color: "light skin",
+        url: "https://img1.g-star.com/product/c_fill,f_auto,h_675,q_80/v1583927558/D17299-C332-B436-Z01W/g-star-raw-high-waist-culotte-sweatpants-pink.jpg"
+      }
+    ],
+    disc: "A broad elasticated waist offers an integrated drawstring. The High Waist Culotte sweatpants is designed in a wide flared fit and a high waist. Inset pockets are added. The leghems show a raw edge finish.",
+    productURL: "https://www.g-star.com/en_us/shop/women/pants/d17299-c332-b436",
 
   },
   {
-      id: 02,
-      name: "Floral-Print Silk Twill Cropped Pajama Culottes",
-      nameD: "Gucci",        
-      price:1500.00,
-      model:"Fashion Culotte",
-      size: [2,4,6],
-      imgUrl:
-      [
-          {
-              color: "light",
-              url:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQjqy_G8BxwPDP0rgUXnQAZiN3TdhX4n3N06VFYtTLHMWORuSBw50xZUa245OcOTVEyikwAValJ&usqp=CAc"
-          },
+    id: 02,
+    name: "Floral-Print Silk Twill Cropped Pajama Culottes",
+    nameD: "Gucci",
+    price: 1500.00,
+    model: "Fashion Culotte",
+    size: [2, 4, 6],
+    imgUrl: [{
+        color: "light",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQjqy_G8BxwPDP0rgUXnQAZiN3TdhX4n3N06VFYtTLHMWORuSBw50xZUa245OcOTVEyikwAValJ&usqp=CAc"
+      },
 
-      ],
-      disc: "Floral-print silk twill pajama culottes. Wide legs. Relaxed fit. Knee-length. made in Italy.",
-      productURL: "https://www.neimanmarcus.com/p/gucci-floral-print-silk-twill-cropped-pajama-culottes-prod211240215?utm_source=google_shopping&adpos=&scid=scplpsku179331122&sc_intid=sku179331122&ecid=NMCS__GooglePLA&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pRhK4SBzlvxuhAyJf3rIR5arB39tLsI9qxcAXFFg558Xic5mQ5836EaAjDAEALw_wcB&gclsrc=aw.ds",
-
-  },          
-  {
-      id: 03,
-      name: "Brooklyn Wide Leg Crop Pant",
-      nameD: "Gucci",        
-      price:89.00,
-      model:"Fashion Culotte",
-      size: [2,4,6],
-      imgUrl:
-      [
-          {
-              color: "Cognac Brown",
-              url:"https://www1.assets-gap.com/webcontent/0018/546/878/cn18546878.jpg",
-          },
-          {
-              color: "Black",
-              url:"https://www1.assets-gap.com/webcontent/0018/546/959/cn18546959.jpg",
-          },
-          {
-              color: "Abalone Grey",
-              url:"https://www1.assets-gap.com/webcontent/0018/563/919/cn18563919.jpg",
-          },    
-          {
-              color: "Navy",
-              url:"https://www1.assets-gap.com/webcontent/0018/717/333/cn18717333.jpg",
-          },                        
-      ],
-      disc: "FIT: Semi-fitted, skims easily over the body with a mid-rise waistband. FOR: Commuting, work and travel. FEEL: Featherweight Stretch™ fabric made with recycled polyester feels silky sleek and light as air. FAVE: Five pockets; 2 front hand, 3 secure zip.",
-      productURL: "https://www.neimanmarcus.com/p/gucci-floral-print-silk-twill-cropped-pajama-culottes-prod211240215?utm_source=google_shopping&adpos=&scid=scplpsku179331122&sc_intid=sku179331122&ecid=NMCS__GooglePLA&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pRhK4SBzlvxuhAyJf3rIR5arB39tLsI9qxcAXFFg558Xic5mQ5836EaAjDAEALw_wcB&gclsrc=aw.ds",
+    ],
+    disc: "Floral-print silk twill pajama culottes. Wide legs. Relaxed fit. Knee-length. made in Italy.",
+    productURL: "https://www.neimanmarcus.com/p/gucci-floral-print-silk-twill-cropped-pajama-culottes-prod211240215?utm_source=google_shopping&adpos=&scid=scplpsku179331122&sc_intid=sku179331122&ecid=NMCS__GooglePLA&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pRhK4SBzlvxuhAyJf3rIR5arB39tLsI9qxcAXFFg558Xic5mQ5836EaAjDAEALw_wcB&gclsrc=aw.ds",
 
   },
   {
-      id: 04,
-      name: "Summer Women's Skirt Pant Ruffle Palazzo",
-      nameD: "Summer Women's Skirt Pant Ruffle Palazzo Trouser Boho Wide Leg Loose Skirt Pants",        
-      price:20.00,
-      model:"Fashion Culotte",
-      size: [2,4,6,8],
-      imgUrl:
-      [
-          {
-              color: "grey",
-              url:"https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-2-large",
-          },
-          {
-              color: "Black",
-              url:"https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-6-large",
-          },
-          {
-              color: "red",
-              url:"https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-7-large",
-          },    
-          {
-              color: "Navy Blue",
-              url:"https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-5-large",
-          },                        
-      ],
-      disc: "Summer Women's Skirt Pant Ruffle Palazzo Trouser Boho Wide Leg Loose Skirt Pants.",
-      productURL: "https://www.wish.com/product/5da51719eac01d008ff95556?hide_login_modal=true&from_ad=goog_shopping&_display_country_code=US&_force_currency_code=USD&pid=googleadwords_int&c=%7BcampaignId%7D&ad_cid=5da51719eac01d008ff95556&ad_cc=US&ad_curr=USD&ad_price=20.20&campaign_id=7203534630&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pQfIP3C7d-oXir5J0FahSbNe1g-BW0fK5lK6kbqaIB38Ycpc_HocrkaAquXEALw_wcB&share=web",
+    id: 03,
+    name: "Brooklyn Wide Leg Crop Pant",
+    nameD: "Gucci",
+    price: 89.00,
+    model: "Fashion Culotte",
+    size: [2, 4, 6],
+    imgUrl: [{
+        color: "Cognac Brown",
+        url: "https://www1.assets-gap.com/webcontent/0018/546/878/cn18546878.jpg",
+      },
+      {
+        color: "Black",
+        url: "https://www1.assets-gap.com/webcontent/0018/546/959/cn18546959.jpg",
+      },
+      {
+        color: "Abalone Grey",
+        url: "https://www1.assets-gap.com/webcontent/0018/563/919/cn18563919.jpg",
+      },
+      {
+        color: "Navy",
+        url: "https://www1.assets-gap.com/webcontent/0018/717/333/cn18717333.jpg",
+      },
+    ],
+    disc: "FIT: Semi-fitted, skims easily over the body with a mid-rise waistband. FOR: Commuting, work and travel. FEEL: Featherweight Stretch™ fabric made with recycled polyester feels silky sleek and light as air. FAVE: Five pockets; 2 front hand, 3 secure zip.",
+    productURL: "https://www.neimanmarcus.com/p/gucci-floral-print-silk-twill-cropped-pajama-culottes-prod211240215?utm_source=google_shopping&adpos=&scid=scplpsku179331122&sc_intid=sku179331122&ecid=NMCS__GooglePLA&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pRhK4SBzlvxuhAyJf3rIR5arB39tLsI9qxcAXFFg558Xic5mQ5836EaAjDAEALw_wcB&gclsrc=aw.ds",
 
-  },        
-  
+  },
+  {
+    id: 04,
+    name: "Summer Women's Skirt Pant Ruffle Palazzo",
+    nameD: "Summer Women's Skirt Pant Ruffle Palazzo Trouser Boho Wide Leg Loose Skirt Pants",
+    price: 20.00,
+    model: "Fashion Culotte",
+    size: [2, 4, 6, 8],
+    imgUrl: [{
+        color: "grey",
+        url: "https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-2-large",
+      },
+      {
+        color: "Black",
+        url: "https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-6-large",
+      },
+      {
+        color: "red",
+        url: "https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-7-large",
+      },
+      {
+        color: "Navy Blue",
+        url: "https://contestimg.wish.com/api/webimage/5da51719eac01d008ff95556-5-large",
+      },
+    ],
+    disc: "Summer Women's Skirt Pant Ruffle Palazzo Trouser Boho Wide Leg Loose Skirt Pants.",
+    productURL: "https://www.wish.com/product/5da51719eac01d008ff95556?hide_login_modal=true&from_ad=goog_shopping&_display_country_code=US&_force_currency_code=USD&pid=googleadwords_int&c=%7BcampaignId%7D&ad_cid=5da51719eac01d008ff95556&ad_cc=US&ad_curr=USD&ad_price=20.20&campaign_id=7203534630&gclid=Cj0KCQjw2PP1BRCiARIsAEqv-pQfIP3C7d-oXir5J0FahSbNe1g-BW0fK5lK6kbqaIB38Ycpc_HocrkaAquXEALw_wcB&share=web",
+
+  },
+
 ];
 
-const articles = [
-  {
+const articles = [{
     headline: "J-Law in Flowy Culottes",
     articleImage: 'https://bit.ly/2WKKlbi',
     publishDate: '01/15/20',
@@ -158,7 +141,7 @@ const articleCardBuilder = (arr) => {
   domString = '';
 
   for (let i = 0; i < arr.length; i++) {
-      domString += `
+    domString += `
     <div class="col-5 mx-auto">
     <div class="card mt-3 testcard">
       <div class="card-body p-2">
@@ -178,7 +161,7 @@ const buildProductCard = (arr) => {
   domString = '';
 
   for (let i = 0; i < arr.length; i++) {
-      domString += `
+    domString += `
     <div class="col-6 col-md-3">
     <div class="card testcard">
       <div class="card-body p-2">
@@ -191,27 +174,31 @@ const buildProductCard = (arr) => {
     </div>
     </div>
     `;
-  }  
+  }
   printToDom('#allCards', domString);
 }
 
 // Adding and Show by or filter by models for product below:
 const filterModelCulotteEvent = (event) => {
   const buttonID = event.target.id;
-  if (buttonID === 'historyCulotte'){
-      const history = pants01.filter(mmod => { return mmod.model === "History Culotte";});
-      //myFunctionHide("details");
-      buildProductCard(history);
-  } else if (buttonID === 'fashion'){
-      const fashion = pants01.filter(mmod => { return mmod.model === "Fashion Culotte";});
-      //myFunctionHide("details");
-      buildProductCard(fashion);
-  }else if (buttonID === 'all'){
-      //myFunctionHide("details");
-      buildProductCard(pants01);
-  }else {
-      console.log("pd excuted!");
-      //buildProductDetailcard(pants01);
+  if (buttonID === 'historyCulotte') {
+    const history = pants01.filter(mmod => {
+      return mmod.model === "History Culotte";
+    });
+    //myFunctionHide("details");
+    buildProductCard(history);
+  } else if (buttonID === 'fashion') {
+    const fashion = pants01.filter(mmod => {
+      return mmod.model === "Fashion Culotte";
+    });
+    //myFunctionHide("details");
+    buildProductCard(fashion);
+  } else if (buttonID === 'all') {
+    //myFunctionHide("details");
+    buildProductCard(pants01);
+  } else {
+    console.log("pd excuted!");
+    //buildProductDetailcard(pants01);
   }
 }
 
@@ -223,7 +210,7 @@ const clickEvents = () => {
 }
 
 const init = () => {
-  buildProductCard(pants01);  
+  buildProductCard(pants01);
   articleCardBuilder(articles);
   clickEvents();
 }
